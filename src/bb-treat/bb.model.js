1fd5f70c55cc3f0e2143f5a91b86a1b792a36696
Ashley <ashleyowenspt@gmail.com>
2022-01-25 08:50:14 -0700

    feat: added bed bug treatment schema

--- src/bb-treat/bb.model.js
+++ src/bb-treat/bb.model.js
@@ -1,3 +1,37 @@
 const { instances } = require("gstore-node");
 const gstore = instances.get("default");
 const { Schema } = gstore;
+
+// Create the "BedBugTreatment" schema
+const schema = new Schema(
+	{
+		isRegular: { type: Boolean, required: true },
+		isExtreme: { type: Boolean, required: true },
+		photos: { type: Array },
+		propertyName: { type: String, required: true },
+		address: { type: String, required: true },
+		zipCode: { type: String, required: true },
+		city: { type: String, required: true },
+		technician: { type: String, required: true },
+		madeContact: { type: Boolean, required: true },
+		contactName: { type: String },
+		contactTitle: { type: String },
+		dateCreated: { type: Date, default: gstore.defaultValues.NOW },
+		dateModified: { type: Date },
+		notes: { type: String },
+	},
+	{
+		queries: {
+			readAll: true,
+		},
+	}
+);
+
+// Query shortcut for listing entities
+const listSettings = {
+	order: { property: "dateCreated" },
+};
+schema.queries("list", listSettings);
+
+// Generates "BedBugTreatment" entity kind in Datastore
+module.exports = gstore.model("BedBugTreatment", schema);
