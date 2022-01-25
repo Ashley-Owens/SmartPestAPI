d03a3e92bd4ba0639350d109234dc7482a99ba98
Ashley <ashleyowenspt@gmail.com>
2022-01-25 09:08:29 -0700

    feat: added bbtreatment routes, renamed directory

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const router = (module.exports = require("express").Router());
 
 router.use("/roach", require("./roach/roach.route"));
+router.use("/bedbugtreatment", require("./bedbug-treatment/bb.route"));
