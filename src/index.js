7192ae8c784f1cafc77f5999aa2ddfeba99f0d79
Ashley <ashleyowenspt@gmail.com>
2022-01-25 20:26:44 -0700

    refactor: renamed roach directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const router = (module.exports = require("express").Router());
 
-router.use("/roach-assessment", require("./roach/roach.route"));
+router.use("/roach-assessment", require("./roach-assessment/roach.route"));
 router.use("/bedbug-treatment", require("./bedbug-treatment/bbt.route"));
 router.use("/bedbug-inspection", require("./bedbug-inspection/bbi.route"));
 router.use("/bird-exclusion", require("./bird-exclusion/birdex.route"));
