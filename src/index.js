80e6c21990588191cb954f3d711c23049484d582
Ashley <ashleyowenspt@gmail.com>
2022-01-25 20:16:52 -0700

    refactor: updated URL naming conventions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const router = (module.exports = require("express").Router());
 
-router.use("/roach", require("./roach/roach.route"));
-router.use("/bedbugtreatment", require("./bedbug-treatment/bbt.route"));
-router.use("/bedbuginspection", require("./bedbug-inspection/bbi.route"));
+router.use("/roach-assessment", require("./roach/roach.route"));
+router.use("/bedbug-treatment", require("./bedbug-treatment/bbt.route"));
+router.use("/bedbug-inspection", require("./bedbug-inspection/bbi.route"));
+router.use("/bird-exclusion", require("./bird-exclusion/birdex.route"));
+router.use("/bird-trapping", require("./bird-trapping/bird.route"));
