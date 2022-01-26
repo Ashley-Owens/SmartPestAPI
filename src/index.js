bc81573b38fbd041f0ac479ced7109ef2a994cb3
Ashley <ashleyowenspt@gmail.com>
2022-01-26 11:40:08 -0700

    feat: added mosquito model and route

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ router.use("/bedbug-treatment", require("./bedbug-treatment/bbt.route"));
 router.use("/bird-exclusion", require("./bird-exclusion/birdex.route"));
 router.use("/bird-trapping", require("./bird-trapping/bird.route"));
 router.use("/dewebbing", require("./dewebbing/dewebbing.route"));
-router.use("honeycomb", require("./honeycomb/honeycomb.route"));
+router.use("/honeycomb", require("./honeycomb/honeycomb.route"));
+router.use("/mosquito", require("./mosquito/mosquito.route"));
 router.use("/roach-assessment", require("./roach-assessment/roach.route"));
