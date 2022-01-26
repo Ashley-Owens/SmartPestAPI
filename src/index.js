496ea50296aac512652a157d533b1747584f9fe8
Ashley <ashleyowenspt@gmail.com>
2022-01-26 11:36:22 -0700

    feat: added honeycomb model and route

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ router.use("/bedbug-treatment", require("./bedbug-treatment/bbt.route"));
 router.use("/bird-exclusion", require("./bird-exclusion/birdex.route"));
 router.use("/bird-trapping", require("./bird-trapping/bird.route"));
 router.use("/dewebbing", require("./dewebbing/dewebbing.route"));
+router.use("honeycomb", require("./honeycomb/honeycomb.route"));
 router.use("/roach-assessment", require("./roach-assessment/roach.route"));
