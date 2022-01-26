0840827a0b39e26886fa82e84b25520c6c9f851e
Ashley <ashleyowenspt@gmail.com>
2022-01-26 11:13:36 -0700

    feat: added dewebbing model and routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const router = (module.exports = require("express").Router());
 
-router.use("/roach-assessment", require("./roach-assessment/roach.route"));
-router.use("/bedbug-treatment", require("./bedbug-treatment/bbt.route"));
 router.use("/bedbug-inspection", require("./bedbug-inspection/bbi.route"));
+router.use("/bedbug-treatment", require("./bedbug-treatment/bbt.route"));
 router.use("/bird-exclusion", require("./bird-exclusion/birdex.route"));
 router.use("/bird-trapping", require("./bird-trapping/bird.route"));
+router.use("/dewebbing", require("./dewebbing/dewebbing.route"));
+router.use("/roach-assessment", require("./roach-assessment/roach.route"));
