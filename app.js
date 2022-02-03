6156959442c3e4372a70d39ec047a100d4c79658
Ashley <ashleyowenspt@gmail.com>
2022-02-03 11:03:57 -0700

    feat: added error handling for 404 and 500 statuses

--- app.js
+++ app.js
@@ -6,8 +6,30 @@ const db = require("./src/config/database");
 app.use("/", require("./src/index"));
 app.set("trust proxy", true);
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+// Error handlers: catches 404 and forward to error handler
+app.use(function (req, res, next) {
+	const err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+});
+
+// Development error handler: prints stacktrace
+if (app.get("env") === "development") {
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.json({
+			message: err.message,
+			error: err,
+		});
+	});
+}
+
+// Production error handler: stacktraces not leaked to user
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.json({
+		message: err.message,
+	});
 });
 
 app.listen(PORT, () => {
