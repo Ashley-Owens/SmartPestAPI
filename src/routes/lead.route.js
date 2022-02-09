e5c748eba51b20ac006b106d005c080415f9e4e3
Ashley <ashleyowenspt@gmail.com>
2022-02-09 12:12:48 -0700

    fix: updated controller name in route

--- src/routes/lead.route.js
+++ src/routes/lead.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Controller = require("../../controller/controller");
+const Controller = require("../controller/lead.controller");
 
 // Initializes router
 const router = express.Router();
