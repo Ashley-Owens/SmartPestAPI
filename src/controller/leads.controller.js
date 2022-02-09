d72ed0442c6e3f7115a0d85b32272f67197f14a8
Ashley <ashleyowenspt@gmail.com>
2022-02-09 12:11:06 -0700

    refactor: renamed lead controller

--- src/controller/leads.controller.js
+++ src/controller/leads.controller.js
@@ -1,6 +1,17 @@
 const { instances } = require("gstore-node");
 const gstore = instances.get("default");
 
+/* Returns all entities for the given model */
+// const getAllEntities = async (req, res, Model) => {
+// 	const pageCursor = req.query.cursor;
+// 	try {
+// 		const { entities } = await Model.list({ start: pageCursor });
+// 		return res.status(200).json(entities);
+// 	} catch (err) {
+// 		return res.status(400).json(err);
+// 	}
+// };
+
 /* Returns all leads by status type: New, Open, Won etc. */
 const getLeadsByStatus = async (req, res, status) => {
 	try {
