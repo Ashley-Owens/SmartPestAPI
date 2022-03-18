474131d4ad457916101a9d5de07ad7098c2aa941
Ashley <ashleyowenspt@gmail.com>
2022-03-18 14:44:33 -0700

    feat: configured route protection with Auth0

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const PORT = process.env.PORT || 8081;
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 const app = require("express")();
 const db = require("./src/config/database");
+const { auth } = require("express-openid-connect");
 
 // ** FIX: Enables CORS globally **
 app.use(function (req, res, next) {
@@ -9,6 +11,28 @@ app.use(function (req, res, next) {
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 	res.setHeader("Access-Control-Allow-Credentials", true);
 	next();
+});
+
+const config = {
+	authRequired: true,
+	auth0Logout: true,
+	secret: process.env.AUTH0_CLIENT_SECRET,
+	baseURL: `http://localhost:${PORT}`,
+	clientID: process.env.AUTH0_CLIENT_ID,
+	issuerBaseURL: "https://smart-pest-api.us.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", async (req, res) => {
+	console.log(req);
+	res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/profile", (req, res) => {
+	res.send(JSON.stringify(req.oidc.user));
 });
 
 // Initializes routes
